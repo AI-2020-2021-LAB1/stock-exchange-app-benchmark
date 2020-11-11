6e95b1731716f11af799b3bdc8d423a47dd7b9e4
Calharad <adudek0610@gmail.com>
2020-11-11 20:01:43 +0100

    benchmark library support with management service

--- benchmark-backend/src/main/java/org/project/benchmark/app/config/ObjectMapperConfiguration.java
+++ benchmark-backend/src/main/java/org/project/benchmark/app/config/ObjectMapperConfiguration.java
@@ -1,4 +1,15 @@
 package org.project.benchmark.app.config;
 
-public class ModelMapperConfiguration {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfiguration {
+    @Bean
+    ObjectMapper mapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        //additional config
+        return mapper;
+    }
 }
--- benchmark-backend/src/main/java/org/project/benchmark/app/repository/TestRepository.java
+++ benchmark-backend/src/main/java/org/project/benchmark/app/repository/TestRepository.java
@@ -1,2 +1,9 @@
-package org.project.benchmark.app.repository;public interface TestRepository {
+package org.project.benchmark.app.repository;
+
+import org.project.benchmark.app.entity.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
 }
