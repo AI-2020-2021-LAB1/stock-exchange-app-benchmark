11bf8f99cb76767cf46f692d4a7ac6a1d41cb03e
KLesniak <mlodyrysiek@gmail.com>
2020-11-15 23:09:33 +0100

    Changed mapper

--- benchmark-backend/src/main/java/org/project/benchmark/app/rest/ConfigurationController.java
+++ benchmark-backend/src/main/java/org/project/benchmark/app/rest/ConfigurationController.java
@@ -1,10 +1,10 @@
 package org.project.benchmark.app.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.project.benchmark.app.dto.ConfigurationDTO;
 import org.project.benchmark.app.dto.ErrorResponse;
 import org.project.benchmark.app.service.ConfigurationService;
@@ -24,11 +24,11 @@ import org.springframework.web.bind.annotation.*;
 public class ConfigurationController {
 
     private final ConfigurationService configurationService;
-    private final ModelMapper mapper;
+    private final ObjectMapper mapper;
 
     @GetMapping("/{id}")
     public ResponseEntity<ConfigurationDTO> getConfigurationDetails(@PathVariable Long id) {
-        ConfigurationDTO configurationDTO = mapper.map(configurationService.getConfigurationByID(id),
+        ConfigurationDTO configurationDTO = mapper.convertValue(configurationService.getConfigurationByID(id),
                 ConfigurationDTO.class);
         return new ResponseEntity<>(configurationDTO, HttpStatus.OK);
     }
