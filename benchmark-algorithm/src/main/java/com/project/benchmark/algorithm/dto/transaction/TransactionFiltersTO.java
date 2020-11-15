f4068df72c86844add6f7b4112e81ad4f0042404
Bartosz Kuryło <19bartoszkurylo97@wp.pl>
2020-11-15 15:42:16 +0100

    S005-T97 - added GET method for transactions

---
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/dto/transaction/TransactionFiltersTO.java
@@ -0,0 +1,22 @@
+package com.project.benchmark.algorithm.dto.transaction;
+
+import com.project.benchmark.algorithm.dto.base.PageParams;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class TransactionFiltersTO {
+    private String abbreviation;
+    private Long amount;
+    private Long amountMore;
+    private Long amountLess;
+    private Date date;
+    private String name;
+    private PageParams pageParams;
+    private Double unitPrice;
+    private Double unitPriceMore;
+    private Double unitPriceLess;
+}
