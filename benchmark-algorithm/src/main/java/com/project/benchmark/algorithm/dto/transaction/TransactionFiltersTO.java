62b271d3e2a87d54c459da0e3ffcfb576077f1d8
Bartosz Kuryło <19bartoszkurylo97@wp.pl>
2020-11-16 09:56:58 +0100

    S005-T97 - added GET method for transactions

--- benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/dto/transaction/TransactionFiltersTO.java
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/dto/transaction/TransactionFiltersTO.java
@@ -1,10 +1,11 @@
 package com.project.benchmark.algorithm.dto.transaction;
 
 import com.project.benchmark.algorithm.dto.base.PageParams;
+import com.project.benchmark.algorithm.utils.QueryString;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
@@ -13,7 +14,11 @@ public class TransactionFiltersTO {
     private Long amount;
     private Long amountMore;
     private Long amountLess;
-    private Date date;
+    private OffsetDateTime date;
+    @QueryString("date<")
+    private OffsetDateTime dateLess;
+    @QueryString("date>")
+    private OffsetDateTime dateMore;
     private String name;
     private PageParams pageParams;
     private Double unitPrice;
--- benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/dto/transaction/TransactionTO.java
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/dto/transaction/TransactionTO.java
@@ -4,14 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import com.project.benchmark.algorithm.dto.order.OrderTO;
-import java.util.Date;
+
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
 public class TransactionTO {
     private long amount;
     private OrderTO buyingOrder;
-    private Date date;
+    private OffsetDateTime date;
     private Integer id;
     private OrderTO sellingOrder;
     private Double unitPrice;
