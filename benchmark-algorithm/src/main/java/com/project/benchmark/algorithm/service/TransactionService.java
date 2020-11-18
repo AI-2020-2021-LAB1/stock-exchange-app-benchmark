e434f2b9ab68c05b6925d26a107d116d9f17e096
Bartosz Kuryło <19bartoszkurylo97@wp.pl>
2020-11-18 11:21:14 +0100

    S006-T90 - added GET for a single transaction, added tests

--- benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/service/TransactionService.java
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/service/TransactionService.java
@@ -3,7 +3,6 @@ package com.project.benchmark.algorithm.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.project.benchmark.algorithm.dto.response.ResponseTO;
-import com.project.benchmark.algorithm.dto.stock.StockFiltersTO;
 import com.project.benchmark.algorithm.dto.transaction.TransactionFiltersTO;
 import com.project.benchmark.algorithm.dto.transaction.TransactionTO;
 import com.project.benchmark.algorithm.endpoints.Endpoints;
@@ -12,6 +11,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -22,6 +22,7 @@ import java.util.List;
 public class TransactionService extends BackendCoreService {
 
     private static final String TRANSACTIONS_GET_ALL = Endpoints.address + Endpoints.API_TRANSACTION;
+    private static final String TRANSACTION_SINGLE = Endpoints.address + Endpoints.API_TRANSACTION + "/{id}";
 
     public ResponseTO<List<TransactionTO>> getTransactions(TransactionFiltersTO filters, String authorization) throws IOException {
         Client client = ClientBuilder.newClient();
@@ -40,6 +41,26 @@ public class TransactionService extends BackendCoreService {
             long time = Duration.between(begin, end).toMillis();//calculate time
             var params = new EndpointParameters(TRANSACTIONS_GET_ALL, time, "GET");//additional info
             return resolveData(response, params, new TypeReference<>() {});//get full data
+        } finally {
+            client.close();
+        }
+    }
+
+    public ResponseTO<TransactionTO> getTransactionById(Integer id, String authorization) throws JsonProcessingException {
+        //convert {id} to proper value
+        String url = pathParam(TRANSACTION_SINGLE, "id", id.toString());
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(url);
+        //request-response time start
+        Instant begin = Instant.now();
+        try (Response response = target.request()
+                .accept(MediaType.APPLICATION_JSON)//return data
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + authorization)//token
+                .get()) {
+            Instant end = Instant.now();//stop measuring time
+            long time = Duration.between(begin, end).toMillis();//calculate time
+            var params = new EndpointParameters(url, time, "GET");//additional info
+            return resolveData(response, params, TransactionTO.class);//get full data
         } finally {
             client.close();
         }
--- benchmark-algorithm/src/test/java/com/project/benchmark/algorithm/service/TransactionServiceTest.java
+++ benchmark-algorithm/src/test/java/com/project/benchmark/algorithm/service/TransactionServiceTest.java
@@ -5,27 +5,47 @@ import com.project.benchmark.algorithm.dto.base.PageParams;
 import com.project.benchmark.algorithm.dto.base.SortParams;
 import com.project.benchmark.algorithm.dto.response.ResponseTO;
 import com.project.benchmark.algorithm.dto.transaction.TransactionFiltersTO;
+import com.project.benchmark.algorithm.dto.transaction.TransactionTO;
 import com.project.benchmark.algorithm.dto.user.LoginUserTO;
 import junit.framework.TestCase;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 public class TransactionServiceTest extends TestCase {
 
     TransactionService transactionService = new TransactionService();
     UserService userService = new UserService();
 
-    public void testGetTransactions() throws IOException {
+    private String login() throws JsonProcessingException {
         LoginUserTO user = new LoginUserTO();
-        user.setUsername("aaa@aaa.pl");
-        user.setPassword("Adudek@dud3k");
-        String auth = userService.login(user).getData();
-        assertNotNull(auth);
+        user.setUsername("MarcinNajman@gmail.pl");
+        user.setPassword("MarcinNajman.gmail.pl1");
+        return userService.login(user).getData();
+    }
+
+    private ResponseTO<List<TransactionTO>> getTransactions(String auth) throws IOException {
         TransactionFiltersTO filters = new TransactionFiltersTO();
         SortParams sort = new SortParams("name", true);
         PageParams params = new PageParams(0, 20, Collections.singletonList(sort));
-        var response = transactionService.getTransactions(filters, auth);
+        return transactionService.getTransactions(filters, auth);
+    }
+
+    public void testGetTransactions() throws IOException {
+        String auth = login();
+        assertNotNull(auth);
+        var response = getTransactions(auth);
+        assertNull(response.getError());
+        assertNotNull(response.getData());
+    }
+
+    public void testGetTransactionById() throws IOException {
+        String auth = login();
+        assertNotNull(auth);
+        int randomTransactionId = getTransactions(auth).getData()
+                .stream().mapToInt(TransactionTO::getId).findAny().orElseThrow();
+        ResponseTO<TransactionTO> response = transactionService.getTransactionById(randomTransactionId, auth);
         assertNull(response.getError());
         assertNotNull(response.getData());
     }
