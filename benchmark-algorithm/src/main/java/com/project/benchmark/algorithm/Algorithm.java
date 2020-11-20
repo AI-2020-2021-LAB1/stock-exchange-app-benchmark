21d92998f12dd88c431e683dd63cb27355e90e0a
Bartosz Kuryło <19bartoszkurylo97@wp.pl>
2020-11-20 17:31:57 +0100

    S006-T90 - PULL NOT REQUIRED, suggested improvements to the Algorithm.java using some of the implemented functions

--- benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/Algorithm.java
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/Algorithm.java
@@ -1,6 +1,22 @@
 package com.project.benchmark.algorithm;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import com.project.benchmark.algorithm.dto.base.PageParams;
+import com.project.benchmark.algorithm.dto.base.SortParams;
+import com.project.benchmark.algorithm.dto.response.ResponseTO;
+import com.project.benchmark.algorithm.dto.stock.StockTO;
+import com.project.benchmark.algorithm.service.StockService;
+import com.project.benchmark.algorithm.dto.stock.StockFiltersTO;
+import com.project.benchmark.algorithm.dto.user.RegisterUserTO;
+import com.project.benchmark.algorithm.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.project.benchmark.algorithm.dto.user.LoginUserTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Algorithm {
 
@@ -8,8 +24,73 @@ public class Algorithm {
     static int numOfOperations = 20;
     static int moneyAmount = 1000;
     static boolean register = false;
+    static String firstName;
+    static String lastName;
+    static String email;
+    static String password;
 
-    public static void main(String[] args) {
+    private static String encodePassword(String password) { return new BCryptPasswordEncoder().encode(password); }
+
+    public static void generateUserData (){
+        String www="password";
+        password = (String) encodePassword(www);
+        int leftLimitUpp = 65; // letter 'A'
+        int rightLimitUpp = 90; // letter 'Z'
+        int leftLimitLow = 97; // letter 'a'
+        int rightLimitLow = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        StringBuilder bufferFirstName = new StringBuilder(targetStringLength);
+        int randomLimitedInt = leftLimitUpp + (int) (new Random().nextFloat() * (rightLimitUpp - leftLimitUpp + 1));
+        bufferFirstName.append((char) randomLimitedInt);
+        for (int i = 0; i < targetStringLength; i++) {
+            randomLimitedInt = leftLimitLow + (int) (new Random().nextFloat() * (rightLimitLow - leftLimitLow + 1));
+            bufferFirstName.append((char) randomLimitedInt);
+        }
+        firstName = bufferFirstName.toString();
+
+        StringBuilder bufferSecondName = new StringBuilder(targetStringLength);
+        randomLimitedInt = leftLimitUpp + (int) (new Random().nextFloat() * (rightLimitUpp - leftLimitUpp + 1));
+        bufferSecondName.append((char) randomLimitedInt);
+        for (int i = 0; i < targetStringLength; i++) {
+            randomLimitedInt = leftLimitLow + (int) (new Random().nextFloat() * (rightLimitLow - leftLimitLow + 1));
+            bufferSecondName.append((char) randomLimitedInt);
+        }
+        lastName = bufferSecondName.toString();
+
+        email = firstName + lastName + "@gmail.pl";
+
+    }
+
+    private static void register() throws JsonProcessingException {
+        generateUserData();
+        RegisterUserTO body = new RegisterUserTO();
+        body.setEmail(email);
+        body.setPassword(password);
+        body.setFirstName(firstName);
+        body.setLastName(lastName);
+        UserService userService = new UserService();
+        userService.register(body);
+    }
+
+    private static String login() throws JsonProcessingException {
+        LoginUserTO user = new LoginUserTO();
+        user.setUsername(email);
+        user.setPassword(password);
+        UserService userService = new UserService();
+        return userService.login(user).getData();
+    }
+
+    private static ResponseTO<List<StockTO>> getStocks(String auth) throws IOException {
+        StockFiltersTO filters = new StockFiltersTO();
+        SortParams sort = new SortParams("name", true);
+        PageParams params = new PageParams(0, 20, Collections.singletonList(sort));
+        StockService stockService = new StockService();
+        return stockService.getStocks(filters, auth);
+    }
+
+    public static void main(String[] args) throws IOException {
 
         {//Coby nie przeliczać tego w "if" - możnaby do funkcji //odpowiada za obliczenie poszczególnych progów (1-33, 34-66 itd.)
             algPercentages[1] = algPercentages[0] + algPercentages[1];
@@ -24,14 +105,20 @@ public class Algorithm {
 
         //Wczytywanie danych z komunikatu.
 
-        if (register == true) System.out.println("Rejestracja");   //Rejestracja
-        else System.out.println("Logowanie");   //Logowanie
+        if (register == true) {
+            register();
+            System.out.println("Rejestracja");   //Rejestracja
+        }
+
+        String authorization = login();
+        System.out.println("Logowanie");   //Logowanie
 
         while (numOfOperations > 0) {
             int randomNum = ThreadLocalRandom.current().nextInt(1, 101);
             if (randomNum <= algPercentages[0]) {
+                StockFiltersTO filters = new StockFiltersTO();
+                getStocks(authorization);
                 System.out.println("Sprawdzenie listy dostępnych akcji");
-                //Sprawdzenie listy dostępnych akcji
                 numOfOperations--;
                 randomNum = ThreadLocalRandom.current().nextInt(1, 101);
                 if (randomNum <= algPercentages[4]) {
