e0a4c381d9e09d72ef19a15ce784c97efa6a21d1
Paweł Kocan <148833@stud.prz.edu.pl>
2020-12-04 23:21:09 +0100

    S005-T71 Added Endpoints

---
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/endpoints/Endpoints.java
@@ -0,0 +1,47 @@
+package com.project.benchmark.algorithm.endpoints;
+
+public class Endpoints {
+    //Adres do serwisu
+    String address = "{host:port}";
+
+    //Token logowania [GET]
+    String OAUTH_TOKEN = "/oauth/token";
+
+    // oauth [POST]
+    String OAUTH = "/oauth";
+
+    //Endpoint do rejestracji nowego użytkownika [POST]
+    String API_REGISTER = "/api/register";
+
+    /* Endpoint do listy akcji [GET] [POST]
+    Pozostałe endpointy:
+    /api/stock/{id} - [GET] [PATCH] - pobranie i aktualizowanie konkretnej akcji
+    /api/stock/{id}/index - [GET] - pobranie historii indeksowania akcji    */
+    String API_STOCK = "/api/stock";
+
+    /* Endpoint do listy zleceń [GET]
+    Pozostałe endpointy:
+    /api/order/{id} - [GET] - pobranie konkretnego zlecenia
+    /api/order/{id}/deactivation - [POST] - dezaktywacja zlecenia
+    /api/order/{id}/transactions - [GET] - pobranie transakcji zlecenia     */
+    String API_ORDER = "/api/order";
+
+    /* Endpoint do transakcji [GET]
+    Pozostałe endpointy:
+    /api/transaction/{id} - [GET] - pobranie konkretnej transakcji    */
+    String API_TRANSACTION = "/api/transaction";
+
+    /* Endpoint do listy użytkowników [GET]
+    Pozostałe endpointy:
+    /api/user/{id} - [GET] - pobranie konkretnego użytkownika
+    /api/user/{id}/order - [GET] - pobranie listy zleceń użytkownika
+    /api/user/{id}/stock - [GET] - pobranie listy akcji użytkownika
+     */
+    String API_USER = "/api/user";
+
+    // Posiadane zlecenia uzytkownika [GET]
+    String USER_OWNED_ORDERS = "/api/user/order/owned";
+
+    // Transakcje użytkownika [GET]
+    String USER_OWNED_TRANSACTIONS = "/api/user/transaction/owned";
+}
