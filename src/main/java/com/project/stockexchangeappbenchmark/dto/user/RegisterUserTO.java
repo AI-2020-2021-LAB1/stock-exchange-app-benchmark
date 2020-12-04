caba141663d76079218de6559375cb673057e11c
Paweł Kocan <148833@stud.prz.edu.pl>
2020-12-04 22:14:36 +0100

    created first version of backend connection. Implemented login and register POST requests

---
+++ src/main/java/com/project/stockexchangeappbenchmark/dto/user/RegisterUserTO.java
@@ -0,0 +1,13 @@
+package com.project.stockexchangeappbenchmark.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserTO {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
