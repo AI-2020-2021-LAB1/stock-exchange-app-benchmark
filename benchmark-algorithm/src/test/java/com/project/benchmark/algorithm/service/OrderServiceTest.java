6712971f373d64ebc88b1a800f7d3d474d04f039
Paweł Kocan <148833@stud.prz.edu.pl>
2020-12-04 23:29:15 +0100

    S005-T90 Added GET request for single order and test

--- benchmark-algorithm/src/test/java/com/project/benchmark/algorithm/service/OrderServiceTest.java
+++ benchmark-algorithm/src/test/java/com/project/benchmark/algorithm/service/OrderServiceTest.java
@@ -1,30 +1,51 @@
 package com.project.benchmark.algorithm.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.project.benchmark.algorithm.dto.base.PageParams;
 import com.project.benchmark.algorithm.dto.base.SortParams;
 import com.project.benchmark.algorithm.dto.order.OrderFiltersTO;
-import com.project.benchmark.algorithm.dto.stock.StockFiltersTO;
+import com.project.benchmark.algorithm.dto.response.ResponseTO;
+import com.project.benchmark.algorithm.dto.order.OrderTO;
 import com.project.benchmark.algorithm.dto.user.LoginUserTO;
 import junit.framework.TestCase;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 public class OrderServiceTest extends TestCase {
 
     OrderService orderService = new OrderService();
     UserService userService = new UserService();
 
-    public void testGetOrders() throws IOException {
+    private String login() throws JsonProcessingException {
         LoginUserTO user = new LoginUserTO();
-        user.setUsername("aaa@aaa.pl");
-        user.setPassword("Adudek@dud3k");
-        String auth = userService.login(user).getData();
-        assertNotNull(auth);
+        user.setUsername("MarcinNajman@gmail.pl");
+        user.setPassword("MarcinNajman.gmail.pl1");
+        return userService.login(user).getData();
+    }
+
+    private ResponseTO<List<OrderTO>> getOrders(String auth) throws IOException {
         OrderFiltersTO filters = new OrderFiltersTO();
         SortParams sort = new SortParams("name", true);
         PageParams params = new PageParams(0, 20, Collections.singletonList(sort));
-        var response = orderService.getOrders(filters, auth);
+        return orderService.getOrders(filters, auth);
+    }
+
+    public void testGetOrders() throws IOException {
+        String auth = login();
+        assertNotNull(auth);
+        var response = getOrders(auth);
+        assertNull(response.getError());
+        assertNotNull(response.getData());
+    }
+
+    public void testGetOrderById() throws IOException {
+        String auth = login();
+        assertNotNull(auth);
+        int randomOrderId = getOrders(auth).getData()
+                .stream().mapToInt(OrderTO::getId).findAny().orElseThrow();
+        ResponseTO<OrderTO> response = orderService.getOrderById(randomOrderId, auth);
         assertNull(response.getError());
         assertNotNull(response.getData());
     }
