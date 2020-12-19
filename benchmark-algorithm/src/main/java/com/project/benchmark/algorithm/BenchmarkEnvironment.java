2812dd5a1a0a4b081e434687f23a8a8426e76c9f
Calharad <adudek0610@gmail.com>
2020-12-20 00:25:21 +0100

    responses endpoint now returns backend cpu ram usage

--- benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/BenchmarkEnvironment.java
+++ benchmark-algorithm/src/main/java/com/project/benchmark/algorithm/BenchmarkEnvironment.java
@@ -24,7 +24,6 @@ import javax.ws.rs.core.Response;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class BenchmarkEnvironment {
