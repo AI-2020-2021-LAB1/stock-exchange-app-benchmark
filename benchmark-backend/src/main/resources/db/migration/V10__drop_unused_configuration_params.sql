d65599fbe2990bd725a7d03efdfeda6778d30c7a
Calharad <adudek0610@gmail.com>
2020-12-20 19:33:10 +0100

    added missing classes

---
+++ benchmark-backend/src/main/resources/db/migration/V10__drop_unused_configuration_params.sql
@@ -0,0 +1,3 @@
+alter table configurations drop column if exists certainty_level;
+alter table configurations drop column if exists no_of_money;
+alter table configurations drop column if exists archived;
